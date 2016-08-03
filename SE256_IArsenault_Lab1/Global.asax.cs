8fb430f1d094db2c578309c612fb8918a8912118
Ian <iaarsenault@email.neit.edu>
2016-08-03 16:18:30 -0400

    updates to fonts, login, and more

---
+++ SE256_IArsenault_Lab1/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Routing;
+
+namespace SE256_IArsenault_Lab1
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            //ignore Websource.axd file
+            routes.Ignore("{resource}.axd/{*pathInfo}");
+            // map static pages
+            routes.MapPageRoute("Home", "Home", "~/Default.aspx");
+            routes.MapPageRoute("Login", "Login", "~/Login.aspx");
+
+            routes.MapPageRoute("ForgotPassword", "Forgot-Password", "~/ForgotPassword.aspx");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
