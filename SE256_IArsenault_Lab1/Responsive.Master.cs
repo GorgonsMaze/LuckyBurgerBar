d2b4c8cd90b72fc3ee69fe92994c5aea432e4b61
Ian <iaarsenault@email.neit.edu>
2016-08-31 23:34:56 -0400

    Updates:  Session ID and Cookies added - continue  Request:Cookies value

--- SE256_IArsenault_Lab1/Responsive.Master.cs
+++ SE256_IArsenault_Lab1/Responsive.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 namespace SE256_IArsenault_Lab1
 {
@@ -11,7 +12,62 @@ namespace SE256_IArsenault_Lab1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Check to see if the browser has a valid authentication ticket
+            // for current users session. If so, manipulate the login and logout buttons 
+            // and welcome message
+            {
+                if (Request.IsAuthenticated)
+                {
+                    // If logged in - hide the login button
+                    lbtnLogIn.Visible = false;
+                    // If logged in we can show the logout button
+                    lbtnLogOut.Visible = true;
+                    // Generate a personalized text message to display on the site
+                    // Grab the cookie
+                    if (Request.Cookies["Fullname"] != null)
+                    {
+                        //lblGreeting.Text = Request.Cookies["Fullname"].Value.ToString();
+                        lblGreeting.Text = String.Concat("Welcome ", Request.Cookies["Fullname"].Value.ToString());
+                        //HttpCookie aCookie = Request.Cookies["Fullname"];
+                        //lblGreeting.Text = "Welcome Fartstick";
+                        // enable menu when user is authenticated
+                    }
+                }
+                else
+                {
+                    // since we have no been authenticated, we have to provide a link to login with
+                    lbtnLogIn.Visible = true;
+                    // Hide the logout button since the user is not authenticated
+                    lbtnLogOut.Visible = false;
+                    // Generate a generic text message to display on the site
+                    lblGreeting.Text = "Welcome Stranger!";
+                }
+
+            }
+        }
+
+  
+        protected void lbtnLogIn_Click(object sender, EventArgs e)
+        {
+            // When the Login button is clicked - send them to login page
+            Response.Redirect("~/Sign-In");
+
 
         }
+
+        protected void lbtnLogOut_Click(object sender, EventArgs e)
+            // Log the user off from Cookie Authentication System
+            // (.NET built in security system )
+        {
+
+            FormsAuthentication.SignOut();
+            // Set name session variable to nothing
+            Session["Fullname"] = null;
+
+            // When the Logout button is clicked - Log them out - send back home
+            Response.Redirect("~/Home");
+
+        }
+
     }
 }
--- SE256_IArsenault_Lab1/Responsive.Master.designer.cs
+++ SE256_IArsenault_Lab1/Responsive.Master.designer.cs
@@ -31,6 +31,33 @@ namespace SE256_IArsenault_Lab1 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// lblGreeting control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblGreeting;
+        
+        /// <summary>
+        /// lbtnLogIn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbtnLogIn;
+        
+        /// <summary>
+        /// lbtnLogOut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbtnLogOut;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
